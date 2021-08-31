bf3aa86a83b661013673698a1a531048951ae6d3
Nima Alamatsaz <nima.alamatsaz@gmail.com>
2021-08-31 10:14:36 -0400

    Update copyright Cleanup comments

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 // google analytics
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'G-W4NCHJK0BE');
 
-// expand .item on click
-$(document).on('click', '.item.arrow', function() {
-  $(this).toggleClass('expand');
-  more = $(this).find('.more').toggleClass('expand')[0];
-  if (more.style.maxHeight) more.style.maxHeight = null;
-  else more.style.maxHeight = more.scrollHeight + 'px';
-});
+$(document).ready(function(event) {
+  // expand .item on click
+  $('.item.arrow').click(function() {
+    $(this).toggleClass('expand');
+    more = $(this).find('.more').toggleClass('expand')[0];
+    if (more.style.maxHeight) more.style.maxHeight = null;
+    else more.style.maxHeight = more.scrollHeight + 'px';
+  });
 
-// maintain normal link behavior in .item children
-$(document).on('click', '.item a', function(event) {
-  event.stopImmediatePropagation();
+  // maintain normal link behavior in .item children
+  $('.item a').click(function(event) {
+    event.stopImmediatePropagation();
+  });
 });
